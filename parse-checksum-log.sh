2f016adc88abcb9645b95e9a60323f1023c970e5
guli <guli@zorkV>
2014-03-08 16:54:43 -0200

    File renamed. Parameter check added.

--- parse-checksum-log.sh
+++ parse-checksum-log.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+    echo "usage: parse-cheksum-log.sh <pt-table-checksum-output-file>"
+    exit 1
+fi
+
+if [ $# -gt 1 ]; then
+    echo "[warning] more than one argument passed, ignoring all but first."
+fi
+
 CHECKSUM_FILE=$1
 
 echo "###### MAX REPLICA LAG: ######"
